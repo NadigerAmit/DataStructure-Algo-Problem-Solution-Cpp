9e9d8ed3bc3e8e4049c92ea562171ce8ac26e049
AmithNadiger <amith.nadiger@gmail.com>
2019-03-22 19:49:23 +0900

    Update indeed_Compiling.cpp
    Compiling code

--- Trees/indeed_Compiling.cpp
+++ Trees/indeed_Compiling.cpp
@@ -46,6 +46,8 @@ struct Node {
 Node* leastWeightNode = nullptr ;
 int LeastWeight = 99999;
 
+
+// Better solution , without extra DS , i.e MAP.
 void getLeastWieghtLeafNode(Node* root,int weight) {
     if(root == nullptr ) return ;
     if(root->children.size() == 0) { // this is leaf node.
@@ -54,34 +56,38 @@ void getLeastWieghtLeafNode(Node* root,int weight) {
 			leastWeightNode = root;
 		}
         return ;
-    }
-    
+    }   
     for(int  i = 0;i<root->children.size();i++) {
-		if(weight+root->children[i]>LeastWeight) return ;
+		if(weight+root->children[i].cost>LeastWeight) return ;
         getLeastWieghtLeafNode(root->children[i].target,weight+root->children[i].cost);
-    }
-    
+    }   
 }
 
+// Below solution uses MAP.
 map<int,Node*> weightMap;
 void  getWieghtOfLeaf(Node* root,int weight) {
-    if(root == nullptr ) return ;
+    if(root == nullptr ) return;
     if(root->children.size() == 0) {
         weightMap.insert(make_pair(weight,root));
         return ;
-    }
-    
+    }    
     for(int  i = 0;i<root->children.size();i++) {
         getWieghtOfLeaf(root->children[i].target,weight+root->children[i].cost);
     }
-    
 }
 
 Node* findMinLeaf(Node* root) {
     // TODO
-    getWieghtOfLeaf(root,0);
+    getWieghtOfLeaf(root,0);  // caling with Map functionality
+	getLeastWieghtLeafNode(root,0); // Better solution , without map.
     auto itr = weightMap.begin();
     if(itr == weightMap.end()) return nullptr;
     return itr->second;
-    
+}
+
+int main() {
+	Node* root = nullptr;
+	// Create the N-ry tree as root node;
+	Node* leafNodeWithMinimumRoot = findMinLeaf(root);
+	return 0;
 }
