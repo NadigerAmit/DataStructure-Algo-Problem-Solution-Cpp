785a81788d53305b4ec38c3257b27f7f0152df25
NadigerAmit <amith.nadiger@gmail.com>
2019-04-30 16:13:06 +0900

    Update SharedPointer.cpp

--- C++17-Practice/SharedPointer.cpp
+++ C++17-Practice/SharedPointer.cpp
@@ -1,3 +1,26 @@
+
+/*
+What is std::shared_ptr<> ?
+shared_ptr is a kind of Smart Pointer class provided by c++11, that is smart enough to automatically delete the associated pointer
+when its not used anywhere. Thus helps us to completely remove the problem of memory leaks and dangling Pointers.
+
+shared_ptr and Shared Ownership:
+It follows the concept of Shared Ownership i.e. different shared_ptr objects can be associated with same pointer and internally
+uses the reference counting mechanism to achieve this.
+
+Each shared_ptr object internally points to two memory locations,
+1.) Pointer to the object
+
+2.) Pointer to control data that is used for reference counting
+
+How Shared Ownership works with the help of Reference Counting:
+When a new shared_ptr object is associated with a pointer,then in its constructor it increases the refernce count associated 
+with this pointer by 1.
+
+When any shared_ptr object goes out of scope then in its destructor it decrements the reference count of the associated pointer by 1.
+If reference count becomes 0 then it means no other shared_ptr object is associated with this memory, in that case it deletes that 
+memory using “delete” function.
+*/
 #include<stdio.h>
 #include<iostream>
 #include<memory>
