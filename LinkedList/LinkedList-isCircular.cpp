06ebdd16889195e2b790a075dc7835b6ca043869
NadigerAmit <amith.nadiger@gmail.com>
2019-05-02 22:40:06 +0900

    Update LinkedList-isCircular.cpp

--- LinkedList/LinkedList-isCircular.cpp
+++ LinkedList/LinkedList-isCircular.cpp
@@ -23,19 +23,19 @@ static Node_t* createCircularLinkedListWithArray(int* array,int size,Node_t **ta
 	Node_t* tempNode = NULL;
 	Node_t* linkerNode = NULL;  // Linker Node in Circle.
     for(int i = 0;i<size;i++) {
-		if(i == 0) {
-		    tempNode = getNode(array[i]);
-			root = tempNode;
-		} else {
-			tempNode->next = getNode(array[i]);
-			tempNode = tempNode->next;
-			if(i == 4) {    // Creating the linked list at 5th position .
-			    linkerNode = tempNode;
-			}
+        if(i == 0) {
+	    tempNode = getNode(array[i]);
+	    root = tempNode;
+	    } else {
+		tempNode->next = getNode(array[i]);
+		tempNode = tempNode->next;
+                if(i == 4) {    // Creating the linked list at 5th position .
+		    linkerNode = tempNode;
 		}
-		*tail = tempNode;
 	}
-	(*tail)->next = linkerNode;   // Creating the circle in the linked list.
+	*tail = tempNode;
+    }
+    (*tail)->next = linkerNode;   // Creating the circle in the linked list.
     return root;
 }
 
@@ -57,84 +57,25 @@ static Node_t* createLinkedListWithArray(int* array,int size,Node_t **tail) {
     return root;
 }
 
-class list {
-	private: int size;
-	private: Node_t* head;
-	private: Node_t* tail;
-	private: static Node_t* getNode(int data);
-	private: static Node_t* createLinkedListWithArray1(int* array,int size,Node_t **tail);
-	public :
-	list(int* array,int numofElements);
-	list();
-	int sizeOfList();
-	void insert(int data);
-};
-list::list() {
-	size = 0;
-	head = tail = NULL;
-}
-list::list(int* array, int numofElements) {
-	size = numofElements;
-	head = createLinkedListWithArray(array,numofElements,&tail);
-	if(size <= 0) return;
-	int i = 0;
-	head = getNode(array[i]);
-	Node_t* tempNode = head;
-	for(i =1;i<size;i++) {
-		tempNode->next = getNode(array[i]);
-		tempNode = tempNode->next;
-	}
-	tail = tempNode;
-}
-
-Node_t* list::createLinkedListWithArray1(int* array,int size,Node_t **tail) {
-    if(size <=0) return NULL;
-	Node_t* root = NULL;
-	Node_t* tempNode = NULL;
-    for(int i=0;i<size;i++) {
-		if(i == 0) {
-		    tempNode = getNode(array[i]);
-			root = tempNode;
-		} else {
-			tempNode->next = getNode(array[i]);
-			tempNode = tempNode->next;
-		}
-		*tail = tempNode;
-	}
-    return root;
-}
-
-int list::sizeOfList() {
-	return size;
-}
-
-Node_t* list::getNode(int data) {
-    Node_t *ptr = (Node_t*)malloc(sizeof(Node_t));
-	assert(ptr != NULL);
-	ptr->data = data;
-	ptr->prev = NULL;
-	ptr->next = NULL;
-	return ptr;
-}
 
 bool isListCircular(Node_t* root) {
-	Node_t* first = root;
-	Node_t* second = root;
-	while(first != NULL && first->next != NULL) {
-		first=first->next->next;
-		second = second->next;
-		if(first == second) {
-			Node_t* initial = root;
-			Node_t* againFirst = first;
-			while(initial != againFirst) {
-				initial = initial->next;
-				againFirst = againFirst->next;
-			}
-			printf("\nLinked node = %d",initial->data);
-			return true;
+    Node_t* first = root;
+    Node_t* second = root;
+    while(first != NULL && first->next != NULL) {
+	first=first->next->next;
+	second = second->next;
+	if(first == second) {
+		Node_t* initial = root;
+		Node_t* againFirst = first;
+		while(initial != againFirst) {
+			initial = initial->next;
+			againFirst = againFirst->next;
 		}
+		printf("\nLinked node = %d",initial->data);
+		return true;
 	}
-	return false;
+    }
+    return false;
 }
 
 int main() {
