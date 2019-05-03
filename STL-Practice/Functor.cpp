52568044299ea1a8e4a527baa5b5d568c64cb8a1
NadigerAmit <amith.nadiger@gmail.com>
2019-05-03 13:59:40 +0900

    Update Functor.cpp

--- STL-Practice/Functor.cpp
+++ STL-Practice/Functor.cpp
@@ -2,15 +2,15 @@
 #include<iostream>
 
 class FunctorDemo {  // This functor object .
-	public:
-	    int operator()(int x,int y) {  // This is functor function.
-			return x+y;
-		}
+    public:
+    int operator()(int x,int y) {  // This is functor function.
+        return x+y;
+    }
 };
 
 int main() {
-	int x = 2,y =3;
-	FunctorDemo FunctorObj;
-	printf(" %d + %d = %d",x,y,FunctorObj(x,y));
-	return 0;
+    int x = 2,y =3;
+    FunctorDemo FunctorObj;
+    printf(" %d + %d = %d",x,y,FunctorObj(x,y));
+    return 0;
 }
