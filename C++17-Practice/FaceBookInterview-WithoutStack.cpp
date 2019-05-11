6787614eac994b826de0ba8e012dc9ed4c182d78
NadigerAmit <amith.nadiger@gmail.com>
2019-05-11 22:48:51 +0900

    Add files via upload

---
+++ C++17-Practice/FaceBookInterview-WithoutStack.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+/*
+Balance a string with parentheses. "a(b)" -> "a(b)"; "(((((" -> ""; "(()())" -> "(()())"; ")ab(()" -> "ab()";
+*/
+
+string ForewardBalance(string str) {
+    int count = 0;
+    int i = 0;
+    int len = str.length();
+    while(i<len) {
+        if(str[i] == ')' && count <= 0 ) {
+            str.erase(i,1);
+        } else if(str[i] == '(') {
+            count++;
+        } else if(str[i] == ')') {
+            count--;
+        }
+        i++;
+    }
+    return str;
+}
+
+string ReverseBalance(string str) {
+     int count = 0;
+     int i = str.length()-1;
+     int len = str.length();
+    
+    while(i>=0) {
+        if(str[i] == '(' && count <= 0 ) {
+            str.erase(i,1);
+        } else if(str[i] == ')') {
+            count++;
+        } else if(str[i] == '(') {
+            count--;
+        }
+        i--;
+    }
+    return str;
+}
+string balance(string str) {
+    return ReverseBalance(ForewardBalance(str));
+}
+
+int main() {
+//  printf("\nIs String Balanced = %s\n",balance("(((a)(b)[[{()}]()))]").c_str());
+//    printf("\nBalance tsring %s\n",balance(")()(((a))(").c_str());
+//    printf("\nBalance tsring %s\n",balance(")))(a[(()(((").c_str());
+     printf("\nBalance tsring %s\n",balance(")ab(()").c_str());
+    return 0;
+}
