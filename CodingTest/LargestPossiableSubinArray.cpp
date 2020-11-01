8cc10b9501160b2af6f9ff2093245a6850158d2f
NadigerAmit <amith.nadiger@gmail.com>
2020-11-01 14:30:34 +0900

    Update LargestPossiableSubinArray.cpp

--- CodingTest/LargestPossiableSubinArray.cpp
+++ CodingTest/LargestPossiableSubinArray.cpp
@@ -17,6 +17,32 @@ def max_subarray(numbers):
         best_sum = max(best_sum, current_sum)
     return best_sum
     
+    
+    
+    ***********************************************************************************************************
+    The below algorithm can be modified to keep track of the starting and ending indices of the maximum subarray as well:
+    ***********************************************************************************************************
+    
+    def max_subarray(numbers):
+    """Find a contiguous subarray with the largest sum."""
+    best_sum = 0  # or: float('-inf')
+    best_start = best_end = 0  # or: None
+    current_sum = 0
+    for current_end, x in enumerate(numbers):
+        if current_sum <= 0:
+            # Start a new sequence at the current element
+            current_start = current_end
+            current_sum = x
+        else:
+            # Extend the existing sequence with the current element
+            current_sum += x
+
+        if current_sum > best_sum:
+            best_sum = current_sum
+            best_start = current_start
+            best_end = current_end + 1  # the +1 is to make 'best_end' exclusive
+
+    return best_sum, best_start, best_end
 */
 int max(int a,int b) {
 	return a>b?a:b;
