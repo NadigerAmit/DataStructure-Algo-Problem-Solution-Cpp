7a84f9753358f35397fb1c5f10f33c55c21e61ad
Amit Nadiger <amith.nadiger@gmail.com>
2022-09-12 22:40:16 +0530

    Update ThreadSyncViaC++MutexAndCondVariable.cpp

--- C++17-Practice/Threads/ThreadSyncViaC++MutexAndCondVariable.cpp
+++ C++17-Practice/Threads/ThreadSyncViaC++MutexAndCondVariable.cpp
@@ -1,3 +1,9 @@
+/*
+Below program demonstrate the thread syncronization using C++ 11 -> mutex and condition variable 
+Thread 1 prints even number starting from 0 by increating the shared global varaible  global.
+Thread 2 prints odd number starting from 1 by increating the same shared global varaible  global..
+
+*/
 #include<stdio.h>
 #include<thread>
 #include<iostream>
@@ -5,6 +11,7 @@
 #include <mutex>
 #include <condition_variable> // std::condition_variable
 using namespace std;
+int global = 0;
 
 std::mutex mtx;
 std::condition_variable cv;
@@ -13,7 +20,7 @@ void threadFun1() {
 	std::unique_lock<std::mutex> lck(mtx,std::defer_lock);
     while(1) {
         lck.lock();
-           printf("Om Shree Ganeshaya Namah\n");
+           printf("Thread 1 - Even %d \n",global++);
 	       cv.notify_all();
 	       cv.wait(lck);
 	   lck.unlock();;
@@ -24,7 +31,7 @@ void threadFun2() {
 	std::unique_lock<std::mutex> lck(mtx,std::defer_lock);
     while(1) {
          lck.lock();
-             printf("Om Namah Shivaya\n");
+             printf("Thread 2 - Odd %d \n",global++);
 		     cv.notify_all();
 	         cv.wait(lck);
 	     lck.unlock();
@@ -32,15 +39,9 @@ void threadFun2() {
 }
 
 int main() {
-
-   // std::unique_lock lk(mutex,std::defer_lock);
-    //pthread_cond_init(&cond,nullptr);
     std::thread thread1(threadFun1);
     std::thread thread2(threadFun2);
 	thread1.join();
 	thread2.join();
-
     return 0;
 }
-
-
