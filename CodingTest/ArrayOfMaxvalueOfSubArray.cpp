18170d074d7ccdc70b94c1a14393948d39f8ecb7
Amit Nadiger <amith.nadiger@gmail.com>
2025-03-09 23:08:35 +0900

    Update ArrayOfMaxvalueOfSubArray.cpp

--- CodingTest/ArrayOfMaxvalueOfSubArray.cpp
+++ CodingTest/ArrayOfMaxvalueOfSubArray.cpp
@@ -21,6 +21,48 @@ You can simply print them out as you compute them.
 
 using namespace std;
 
+
+///////////////////////////////////////////////////
+// Most simplest solution 
+int maxOfThreeElements(int a , int b , int c) {
+   // cout<<"a "<< a<<endl;
+   // cout<<"b "<< b<<endl;
+   // cout<<"c "<< c<<endl;
+    if(a>b) {
+        if(a>c) {
+            return a;
+        }
+    } else {
+        if(b>c) {
+            return b;
+        }
+    }
+    return c;
+}
+
+void printVector(vector<int>& v) {
+    for(auto i :v) {
+        cout<<i<<endl;
+    }
+}
+void crerateMaxOfSubArray(vector<int>& v,int k) {
+    int n1,n2,n3;
+    vector<int> result;
+    for(int i = 0; i < v.size(); i++) {
+        if(i+1 > v.size()-1 || i+2 > v.size()-1) {
+            printVector(result);
+            return ;
+        }
+        n1 = v[i];
+        n2 = v[i+1];
+        n3 = v[i+2];
+        int m = maxOfThreeElements(n1,n2,n3);
+        result.push_back(m);
+    }
+}
+
+///////////////////////////////////////////////////
+
 // This is not efficient complexity is  n*k where is arraySize and k is sizeOfsub array. 
 void printMaxOfSubArrayUsingArrayIndex(int array[] , int arraySize ,int k) {
 	if(k>arraySize) return ;
