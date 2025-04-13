f86caecc1463428d4302ba398c9b3ae98ce2102d
Amit Nadiger <amith.nadiger@gmail.com>
2025-04-13 23:48:01 +0900

    Update PriorityQWithPair.cpp

--- STL-Practice/PriorityQWithPair.cpp
+++ STL-Practice/PriorityQWithPair.cpp
@@ -1,3 +1,13 @@
+/*
+https://en.cppreference.com/w/cpp/container/priority_queue
+
+The priority queue is a container adaptor that provides constant time lookup of the largest (by default) element, at the expense of logarithmic insertion and extraction.
+
+A user-provided Compare can be supplied to change the ordering, e.g. using std::greater<T> would cause the smallest element to appear as the top().
+
+Working with a priority_queue is similar to managing a heap in some random access container, with the benefit of not being able to accidentally invalidate the heap.
+
+*/
 #include<stdio.h>
 #include<iostream>
 #include<queue>
@@ -23,5 +33,11 @@ int main() {
 	}
 	return 0;
 }
-vector<iPair> pairVect;
-set<iPair> pairSet;
+/*
+5------4
+5------8
+5------9
+8------4
+8------4
+13------4
+*/
